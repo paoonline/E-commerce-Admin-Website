ba491bf191e2141f7d590ae38b98ecf1397cbe01
Nattawut Visuttipinat <nattawut@cupcodeteam.com>
2020-01-04 00:12:35 +0700

    new unit test

--- src/util/config.js
+++ src/util/config.js
@@ -1,6 +1,7 @@
 const config = {
-    service:'https://mighty-springs-75903.herokuapp.com/'
-    // service:'http://localhost:3090'
+    service:'https://mighty-springs-75903.herokuapp.com/',
+    // service:'http://localhost:3090/',
+    token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1ZGQxMzJhNDQyZWQyNTJlODcxMTU0NTEiLCJpYXQiOjE1Nzc2MDM3ODkwMTh9.nfzNsKwDDG334vxFNdq9XlLD5E22W_mvlAV-smoH8Vs'
 };
 
 export default config
